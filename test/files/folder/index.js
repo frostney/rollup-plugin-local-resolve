a4cae04175254ed17d87bbdcc29212fdb92be92f
Johannes Stein <johannesstein@arkham.fritz.box>
2016-02-29 04:03:57 +0100

    Add test

---
+++ test/files/folder/index.js
@@ -0,0 +1,3 @@
+export default function () {
+  console.log('This function is imported from a folder.');
+}
---
+++ test/index.js
@@ -0,0 +1,15 @@
+import test from 'ava';
+
+import { rollup } from 'rollup';
+import localResolve from '../dist/rollup-plugin-local-resolve.js';
+
+test(t =>
+  rollup({
+    entry: './files/index.js',
+    plugins: [localResolve()],
+  }).then(stats => {
+    t.same(stats.modules[0], { id: './files/folder/index.js' });
+    t.same(stats.modules[1], { id: './files/index.js' });
+    t.is(stats.modules.length, 2);
+  })
+);
