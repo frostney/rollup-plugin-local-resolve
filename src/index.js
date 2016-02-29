b73c06cfa37f16690b84b242f1812bf9972d31e5
Johannes Stein <johannesstein@arkham.fritz.box>
2016-02-29 03:43:28 +0100

    Initial version

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { statSync } from 'fs';
+import path from 'path';
+
+export default function localResolver() {
+  return {
+    resolveId(importee, importer) {
+      if (importee && importer) {
+        const basename = path.basename(importer);
+        const directory = importer.split(basename)[0];
+
+        const dirIndexFile = './' + path.join(directory + importee, 'index.js');
+
+        // TODO: This should be asynchronous
+        const stats = statSync(dirIndexFile);
+
+        if (stats.isFile()) {
+          return dirIndexFile;
+        }
+
+        return null;
+      }
+
+      return importee;
+    },
+  };
+}
